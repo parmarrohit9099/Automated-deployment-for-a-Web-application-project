795d56439da539bbdaab1e925a9a523a51c06761
rohit <rohit@.com>
2025-06-02 14:51:23 +0000

    logging

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 @app.route("/")
 def main():
+    logging.info("Home page accessed from IP: %s", request.remote_addr)
     model = {"title":  "Hello! Welcome to Rohit bhai PROJET !!"}
     return render_template('index.html', model=model)
 if __name__ ==  "__main__":
+    logging.info("Starting Flask app on port 8080 . . .")
     app.run(host='0.0.0.0' , port=8080, debug=True, threaded=True)
